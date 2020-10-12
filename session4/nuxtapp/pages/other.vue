44d35cbe77c3afa74b8ae1dc50386a3b62cf4896
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-13 01:56:59 +0900

    nuxt project作成

---
+++ session4/nuxtapp/pages/other.vue
@@ -0,0 +1,20 @@
+<template>
+     <section class="container">
+          <h1>{{title}}</h1>
+          <p>{{message}}</p>
+          <hr>
+          <pre>{{now}}</pre>
+          <router-link to="/">Go to top</router-link>
+     </section>
+</template>
+
+<script>
+export default {
+     data:function() {
+          return {
+               title: 'Other',
+               message: 'これは、別のページです',
+          }
+     }
+}
+</script>
