75903eb7b9cd8005bb0ab51e3e7de5dba6f9f0e4
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-12 17:57:35 +0900

    プロジェクト開発

---
+++ session3/vueapp/src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <h1>{{title}}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+  },
+  data:function() {
+    return {
+      message:"お名前は？",
+      input:"no name",
+    };
+  },
+  methods: {
+    doAction: function() {
+      this.message = 'こんにちわ！' + this.input + 'さん！';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+div {
+  margin: 0px;
+  padding: 0px;
+  text-align: left;
+}
+
+h1 {
+  font-size: 72px;
+  font-weight: bold;
+  text-align: right;
+  letter-spacing: -8px;
+  color: #f0f0f0;
+  margin: 0px;
+}
+
+p {
+  margin: 0px;
+  color: #666;
+  font-size: 16px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
