0fa604e387c45cb2629151de740b0270a1ddf5ef
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-14 17:49:46 +0900

    store

--- session4/nuxtapp/pages/index.vue
+++ session4/nuxtapp/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="container">
     <h1>{{title}}</h1>
-    <p>{{message}}</p>
+    <p>{{$store.state.message}}</p>
     <hr>
-    <router-link to="/other">Go to other</router-link>
+    <div class="link" v-on:click="doAction">
+      <a>Clicked: {{$store.state.counter}}</a>
+    </div>
   </section>
 </template>
 
@@ -13,55 +15,16 @@ export default {
     return {
       title:'Hello',
       message:'This is message',
-      now: 'wait.....'
     };
   },
-  created: function() {
-    setInterval( () => {
-      var d = new Date();
-      this.now = d.getHours() + ':'+ d.getMinutes() + ':' + d.getSeconds();
-    },1000);
-  },
+  methods: {
+    doAction:function() {
+      this.$store.state.counter ++;
+    }
+  }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
